99e8afda1a16c41a6de5c09203875f50447533ce
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-08-15 20:42:28 +0200

    Enums updated with methods

--- src/main/java/model/enums/MeasureName.java
+++ src/main/java/model/enums/MeasureName.java
@@ -1,7 +1,26 @@
 package model.enums;
 
-public enum MeasureName {
+import java.util.HashMap;
+import java.util.Map;
 
+public enum MeasureName {
     GRAM,
-    PIECE
+    PIECE;
+
+    private static Map<MeasureName, Integer> measureNameIntegerMap = new HashMap<>();
+    private static Map<Integer, MeasureName> integerMeasureNameMap = new HashMap<>();
+
+    public static int getMeasureNameIdByEnum(MeasureName measureName) {
+        measureNameIntegerMap.put(MeasureName.GRAM, 1);
+        measureNameIntegerMap.put(MeasureName.PIECE, 2);
+
+        return measureNameIntegerMap.get(measureName);
+    }
+
+    public static MeasureName getMeasureNameEnumById(Integer measureNameId) {
+        integerMeasureNameMap.put(1, MeasureName.GRAM);
+        integerMeasureNameMap.put(2, MeasureName.PIECE);
+
+        return integerMeasureNameMap.get(measureNameId);
+    }
 }
--- src/main/java/model/enums/ProductCategory.java
+++ src/main/java/model/enums/ProductCategory.java
@@ -1,9 +1,32 @@
 package model.enums;
 
-public enum ProductCategory {
+import java.util.HashMap;
+import java.util.Map;
 
+public enum ProductCategory {
     TEA,
     COFFEE,
     FOOD,
-    ACCESSORY
+    ACCESSORY;
+
+    private static Map<ProductCategory, Integer> productCategoryIntegerMap = new HashMap<>();
+    private static Map<Integer, ProductCategory> integerProductCategoryMap = new HashMap<>();
+
+    public static int getProductCategoryIdByEnum(ProductCategory productCategory) {
+        productCategoryIntegerMap.put(ProductCategory.TEA, 1);
+        productCategoryIntegerMap.put(ProductCategory.COFFEE, 2);
+        productCategoryIntegerMap.put(ProductCategory.FOOD, 3);
+        productCategoryIntegerMap.put(ProductCategory.ACCESSORY, 4);
+
+        return productCategoryIntegerMap.get(productCategory);
+    }
+
+    public static ProductCategory getProductCategoryEnumById(Integer productCategoryId) {
+        integerProductCategoryMap.put(1, ProductCategory.TEA);
+        integerProductCategoryMap.put(2, ProductCategory.COFFEE);
+        integerProductCategoryMap.put(3, ProductCategory.FOOD);
+        integerProductCategoryMap.put(4, ProductCategory.ACCESSORY);
+
+        return integerProductCategoryMap.get(productCategoryId);
+    }
 }
