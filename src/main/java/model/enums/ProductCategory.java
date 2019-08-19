38d6ce77eaec75b9eb8ca53b643165c1e4d25fff
Danylo Tkachenko <d.y.tkachenko@gmail.com>
2019-08-19 18:45:29 +0200

    Removed conversion methods from enums; Code shall be independent from DB, hence mapping happens using only DB available values

--- src/main/java/model/enums/ProductCategory.java
+++ src/main/java/model/enums/ProductCategory.java
@@ -1,32 +1,8 @@
 package model.enums;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public enum ProductCategory {
     TEA,
     COFFEE,
     FOOD,
     ACCESSORY;
-
-    private static Map<ProductCategory, Integer> productCategoryIntegerMap = new HashMap<>();
-    private static Map<Integer, ProductCategory> integerProductCategoryMap = new HashMap<>();
-
-    public static int getProductCategoryIdByEnum(ProductCategory productCategory) { // TODO: remove
-        productCategoryIntegerMap.put(ProductCategory.TEA, 1);
-        productCategoryIntegerMap.put(ProductCategory.COFFEE, 2);
-        productCategoryIntegerMap.put(ProductCategory.FOOD, 3);
-        productCategoryIntegerMap.put(ProductCategory.ACCESSORY, 4);
-
-        return productCategoryIntegerMap.get(productCategory);
-    }
-
-    public static ProductCategory getProductCategoryEnumById(Integer productCategoryId) { // TODO: remove
-        integerProductCategoryMap.put(1, ProductCategory.TEA);
-        integerProductCategoryMap.put(2, ProductCategory.COFFEE);
-        integerProductCategoryMap.put(3, ProductCategory.FOOD);
-        integerProductCategoryMap.put(4, ProductCategory.ACCESSORY);
-
-        return integerProductCategoryMap.get(productCategoryId);
-    }
 }
